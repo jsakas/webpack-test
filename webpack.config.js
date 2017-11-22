0473e16edcdb2f1062eb1080db39bbe83b92bb00
Jon Sakas <jon.sakas@beatport.com>
2017-11-22 10:31:01 -0700

    Use outputPath in file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[hash].js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    compress: true,
-    port: 7000
-  },
-  module: {
-    rules: [{
-        test: /\.css$/,
-        use: [{
-          loader: 'file-loader'
-        }, {
-          loader: 'sass-loader',
-        }]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'file-loader'
-        }]
-      }
+const outputPath = 'static/'
+
+module.exports = (env) => {
+  return {
+    entry: [
+      './src/js/index.js'
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[hash].js',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      compress: true,
+      port: 7000
+    },
+    module: {
+      rules: [{
+          test: /\.css$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: outputPath
+            }
+          }, {
+            loader: 'sass-loader',
+          }]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: outputPath
+            }
+          }],
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src/index.html')
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html')
-    })
-  ]
+  }
 };
